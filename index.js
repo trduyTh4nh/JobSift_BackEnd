b6c410669e2dbd54d14475e986af02a584c148c7
Duy Thanh <tttranduy999@gmail.com>
2023-10-21 14:24:49 +0700

    backend code

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const post = require('./post');
 // const userRouter = require('./router/user');
 const pgp = require('pg-promise')();
 const userSignup = require('./models/user');
-const { errors } = require('pg-promise');
+const { errors, as } = require('pg-promise');
 const db = pgp({
     host: 'localhost',
     port: 5432,
@@ -50,9 +50,47 @@ app.use(bodyParser.urlencoded({ extended: false }))
 //     // Rest of your route handling logic
 // });
 
+app.post("/addrating", async (req, res) => {
 
+    //lấy rating ra từ user
+    const { iduser, id_post, numberstar } = req.body
+
+    db.oneOrNone(`SELECT * FROM "rate" where iduser = ${iduser} and id_post = ${id_post}`).
+    then((respone) => {
+
+        if (respone !== null) {
+            db.oneOrNone(`UPDATE "rate" SET numberstar = ${numberstar} WHERE iduser = ${iduser} and  id_post = ${id_post}`)
+                .then(() => {
+                    res.status(200).json({ message: "Successfully!" })
+                })
+                .catch((error) => {
+                    console.log(error)
+                    res.status(500).json({ error: "Failed!" })
+                })
+        }
+        else {
+            db.none('INSERT INTO "rate" (iduser, id_post, numberstar) VALUES ($1, $2, $3)',
+            [iduser, id_post, numberstar])
+            .then(() => {
+                res.status(200).json(req.body);
+                res.status(200).json({ message: "Successfully!" })
+            })
+            .catch((error) => {
+                console.error(error)
+                res.status(500).json({error: "Invalid"})
+            })
+        }
+    })
+    .catch((error) => {
+        console.log(error)
+        res.status(500).json({error: error})
+    })
+    //fetch kết quả
+    //check: nếu kết quả có (lenght > 0) thì update
+    //ngược lại nếu kết quả ko có (length = 0) thì add
+})
 app.post("/adduser", async (req, res) => {
-   
+
     const { email, password, phone, full_name, profile_picture, diamond_count } = req.body;
     console.log("DATA" + req.body)
 
@@ -68,7 +106,7 @@ app.post("/adduser", async (req, res) => {
 
     db.none(insertQR, [email, password, phone, full_name, profile_picture, diamond_count])
         .then(() => {
-         
+
             res.status(200).json(req.body);
         })
         .catch((error) => {
@@ -78,74 +116,151 @@ app.post("/adduser", async (req, res) => {
 });
 
 app.post("/addpost", async (req, res) => {
-  const {
-      tieu_de,
-      nganh_nghe,
-      soluong_nguoi,
-      job_category,
-      dia_chi,
-      job_time,
-      gioi_tinh,
-      luong,
-      trinh_do_hoc_van,
-      kinh_nghiem_yeu_cau,
-      phuc_loi,
-      note,
-      id_ntd,
-  } = req.body;
+    const {
+        tieu_de,
+        nganh_nghe,
+        soluong_nguoi,
+        job_category,
+        dia_chi,
+        job_time,
+        gioi_tinh,
+        luong,
+        trinh_do_hoc_van,
+        kinh_nghiem_yeu_cau,
+        phuc_loi,
+        note,
+        id_ntd,
+    } = req.body;
 
-  const insertQR = `
+    const insertQR = `
       INSERT INTO "post" (tieu_de, nganh_nghe, soluong_nguoi, job_category, dia_chi, job_time, gioi_tinh, luong, trinh_do_hoc_van, kinh_nghiem_yeu_cau, phuc_loi, note, id_ntd)
       VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)
   `;
 
-  db.none(insertQR, [
-      tieu_de,
-      nganh_nghe,
-      soluong_nguoi,
-      job_category,
-      dia_chi,
-      job_time,
-      gioi_tinh,
-      luong,
-      trinh_do_hoc_van,
-      kinh_nghiem_yeu_cau,
-      phuc_loi,
-      note,
-      id_ntd,
-  ])
-      .then(() => {
-          res.status(200).json({ message: 'Post created successfully', data: req.body });
-      })
-      .catch((error) => {
-          console.log(error);
-          res.status(500).json({ error: 'Internal Server Error' });
-      });
+    db.none(insertQR, [
+        tieu_de,
+        nganh_nghe,
+        soluong_nguoi,
+        job_category,
+        dia_chi,
+        job_time,
+        gioi_tinh,
+        luong,
+        trinh_do_hoc_van,
+        kinh_nghiem_yeu_cau,
+        phuc_loi,
+        note,
+        id_ntd,
+    ])
+        .then(() => {
+            res.status(200).json({ message: 'Post created successfully', data: req.body });
+        })
+        .catch((error) => {
+            console.log(error);
+            res.status(500).json({ error: 'Internal Server Error' });
+        });
 });
+
+app.post('/upfeedback', async (req, res) => {
+
+    const { idpost, iduser, content, time } = req.body
+    console.log("IDPOST: " + idpost + "IDUSER: " + iduser + "CONTENT: " + content + "TIME: " + time)
+
+
+    db.none(`INSERT INTO "feedback" (idpost, iduser, content, time)  
+        VALUES ($1, $2, $3, $4)
+        `, [idpost, iduser, content, time])
+        .then(() => {
+            res.status(200).json({ message: "Upload feedback successfully!" })
+        })
+        .catch((error) => {
+            console.log(error)
+            res.status(500).json({ error: "Internal Server Error" })
+        })
+
+})
+
+
+// lấy toàn bộ feed back của post
+app.get('/upfeedback/:idpost', async (req, res) => {
+    const idPost = req.params.idpost
+
+    try {
+        const fbs = await db.manyOrNone(`SELECT fb.* FROM feedback fb, post ps WHERE fb.idpost = ps.id_post and idpost = ${idPost}`);
+        if (!fbs || fbs.length === 0) {
+            res.status(401).json({ error: 'No data found' });
+        }
+        res.status(200).json({ fbs: fbs });
+
+    }
+    catch (error) {
+        console.error(error)
+        res.status(500).json({ error: 'Internal Server Error' })
+    }
+})
+
+app.get('/upfeedback/getrate/:idpost', async (req, res) => {
+    const idpost = req.params.idpost
+
+    try {
+        const rate = await db.manyOrNone(`SELECT sum(numberstar) as start, count(iduser) as user FROM rate WHERE id_post = ${idpost}`);
+        if (!rate) {
+            res.status(401).json({ error: 'No data found' });
+        }
+        res.status(200).json({ rate: rate })
+    }
+    catch (error) {
+        console.error(error)
+        res.status(500).json({ error: 'Internal Server Error' })
+    }
+})
 
 
 
 app.post('/login', async (req, res) => {
     const { email, password } = req.body;
-  
+
     try {
-      const user = await db.oneOrNone('SELECT * FROM "users" WHERE email = $1', [email]);
-  
-      if (!user) {
-        return res.status(401).json({ error: 'Invalid email or password' });
-      }
-  
-      if (password !== user.password) {
-        return res.status(401).json({ error: 'Invalid email or password' });
-      }
-  
-      res.status(200).json({ message: 'Login successful' });
+        const user = await db.oneOrNone('SELECT * FROM "users" WHERE email = $1', [email]);
+
+        if (!user) {
+            return res.status(401).json({ error: 'Invalid email or password' });
+        }
+
+        if (password !== user.password) {
+            return res.status(401).json({ error: 'Invalid email or password' });
+        }
+
+        res.status(200).json({ message: 'Login successful', user: user });
     } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Internal Server Error' });
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
     }
-  });
-  
+});
+
+app.post('/ntd/:idntd', async (req, res) => {
+    const idNtd = req.params.idntd
+    console.log(idNtd)
+    try {
+        const ntd = await db.oneOrNone(`SELECT dn.* FROM nha_tuyen_dung td, doanh_nghiep dn WHERE td.id_dn = dn.id_dn AND id_ntd = ${idNtd}`)
+        if (!ntd) {
+            return res.status(401).json({ error: 'Lỗi data' })
+        }
+
+        res.status(200).json({ ntd: ntd })
+    }
+    catch (error) {
+        console.log(error)
+        res.status(500).json({ error: 'Internal Server Error' })
+    }
+})
+
+
+
+
+
+
+
 
 
 
@@ -160,6 +275,8 @@ app.get('/', (request, result) => {
             result.status(500).send(e);
         });
 });
+
+
 
 
 app.listen(port, () => {
